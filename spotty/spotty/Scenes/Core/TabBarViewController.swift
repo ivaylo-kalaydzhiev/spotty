c6d10c725658ec0327a1b6eddfbb7fcf78de6917
Ivaylo Kalaydzhiev <ivailo.s.k@outlook.com>
2022-06-09 16:16:11 +0300

    Layout project structure

--- spotty/spotty/Scenes/Core/TabBarViewController.swift
+++ spotty/spotty/Scenes/Core/TabBarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TabBarViewController.swift
 //  spotty
 //
 //  Created by Ivaylo Kalaydzhiev on 9.06.22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 

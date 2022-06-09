3f372a5f7266ba37393a8d65b0ef45b156cfd641
Ivaylo Kalaydzhiev <ivailo.s.k@outlook.com>
2022-06-09 15:49:22 +0300

    Add New Project

---
+++ spotty/spotty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spotty
+//
+//  Created by Ivaylo Kalaydzhiev on 9.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

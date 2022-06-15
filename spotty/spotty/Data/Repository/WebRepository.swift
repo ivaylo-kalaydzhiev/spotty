963051c81a97167acef959ee8e35218f3abf153c
Ivaylo Kalaydzhiev <ivailo.s.k@outlook.com>
2022-06-15 13:03:15 +0300

    Implement getRecentlyPlayedTracks()

--- spotty/spotty/Data/Repository/WebRepository.swift
+++ spotty/spotty/Data/Repository/WebRepository.swift
@@ -29,4 +29,37 @@ struct WebRepository {
         let urlRequest = SpotifyEndpoint.getCurrentUserPlaylists(limit: 49).urlRequest
         Network.performAuthorizedRequest(with: urlRequest, completion: completion)
     }
+    
+    func getRecentlyPlayedTracks(completion: @escaping (Result<ItemsResponse<AudioTrackWrapperRequest>, Error>) -> Void) {
+        
+        let fiveDaysAgo: TimeInterval = -432000
+        let date = Date().addingTimeInterval(fiveDaysAgo)
+        let dateAsNumber = Int(date.timeIntervalSince1970)
+        let urlRequest = SpotifyEndpoint.getRecentlyPlayedTracks(limit: 49, since: dateAsNumber).urlRequest
+        Network.performAuthorizedRequest(with: urlRequest, completion: completion)
+    }
 }
+
+//case getCurrentUserProfile
+//case getCurrentUserTopTracks(limit: Int)
+//case getCurrentUserTopArtists(limit: Int)
+//case getCurrentUserPlaylists(limit: Int)
+//case getRecentlyPlayedTracks(limit: Int, since: Int)
+//case getUserSavedEpisodes(limit: Int)
+//case getUserSavedShows(limit: Int)
+//case searchAllItems(limit: Int, query: String)
+//case getFeaturedPlaylists(limit: Int)
+
+//case createPlaylist(userId: String, name: String)
+
+//case getArtist(artistId: String)
+//case getArtistTopTracks(artistId: String)
+//case getShow(showId: String)
+//case getShowEpisodes(showId: String, limit: Int)
+//case getPlaylist(playlistId: String)
+//case getPlaylistTracks(playlistId: String, limit: Int)
+
+//case deleteSongsFromPlaylist(playlistId: String, tracks: [AudioTrackRequest], playlistSnapshotId: String)
+
+//case getTrack(trackId: String)
+//case getEpisode(episodeId: String)

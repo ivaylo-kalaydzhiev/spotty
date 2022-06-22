ff28c5c9ac88283be0e19dfd25b3443b7ce5cc61
Ivaylo Kalaydzhiev <ivailo.s.k@outlook.com>
2022-06-22 12:42:43 +0300

    Write DocC for the Layout DSL

--- spotty/spotty/UIComponents/Constraints/LayoutAnchor.swift
+++ spotty/spotty/UIComponents/Constraints/LayoutAnchor.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-/// Represents a single `NSLayoutConstraint`
+/// Represents a single ``NSLayoutConstraint``
 enum LayoutAnchor {
     
     case constant(attribute: NSLayoutConstraint.Attribute,
@@ -24,32 +24,63 @@ enum LayoutAnchor {
 // MARK: - Factory methods
 extension LayoutAnchor {
     
+    /// Premade anchor, relative to the leading constraint of the superview.
     static let leading = relative(attribute: .leading, relation: .equal, relatedTo: .leading)
+    
+    /// Premade anchor, relative to the trailing constraint of the superview.
     static let trailing = relative(attribute: .trailing, relation: .equal, relatedTo: .trailing)
+    
+    /// Premade anchor, relative to the top constraint of the superview.
     static let top = relative(attribute: .top, relation: .equal, relatedTo: .top)
+    
+    /// Premade anchor, relative to the bottom constraint of the superview.
     static let bottom = relative(attribute: .bottom, relation: .equal, relatedTo: .bottom)
 
+    /// Premade anchor, relative to the X axis constraint of the superview.
     static let centerX = relative(attribute: .centerX, relation: .equal, relatedTo: .centerX)
+    
+    /// Premade anchor, relative to the X axis constraint of the superview.
     static let centerY = relative(attribute: .centerY, relation: .equal, relatedTo: .centerY)
 
+    /// Premade anchor, lets you set a width contraint.
     static let width = constant(attribute: .width, relation: .equal)
+    
+    /// Premade anchor, lets you set a height contraint.
     static let height = constant(attribute: .height, relation: .equal)
 
+    /// Method letting you create a constant anchor.
+    /// - Parameters:
+    ///   - attribute: The attribute you want to constraint.
+    ///   - relation: The type of realtion you want to set.
+    /// - Returns: Layout Anchor
     static func constant(
         attribute: NSLayoutConstraint.Attribute,
         relation: NSLayoutConstraint.Relation) -> (CGFloat) -> LayoutAnchor {
         return { constant in
-            .constant(attribute: attribute, relation: relation, constant: constant)
+            .constant(attribute: attribute,
+                      relation: relation,
+                      constant: constant)
         }
     }
-
+    
+    /// Method letting you create a relative anchor.
+    /// - Parameters:
+    ///   - attribute: The attribute you want to constraint.
+    ///   - relation: The type of realtion you want to set.
+    ///   - relatedTo: The attribute to which you want to relate the anchor.
+    ///   - multiplier: Optionally set a multiplier.
+    /// - Returns: Layout Anchor
     static func relative(
         attribute: NSLayoutConstraint.Attribute,
         relation: NSLayoutConstraint.Relation,
         relatedTo: NSLayoutConstraint.Attribute,
         multiplier: CGFloat = 1) -> (CGFloat) -> LayoutAnchor {
         return { constant in
-            .relative(attribute: attribute, relation: relation, relatedTo: relatedTo, multiplier: multiplier, constant: constant)
+            .relative(attribute: attribute,
+                      relation: relation,
+                      relatedTo: relatedTo,
+                      multiplier: multiplier,
+                      constant: constant)
         }
     }
 }
--- spotty/spotty/UIComponents/Constraints/UIView+Constraints.swift
+++ spotty/spotty/UIComponents/Constraints/UIView+Constraints.swift
@@ -9,12 +9,20 @@ import UIKit
 
 extension UIView {
     
+    /// Add a Subview to a Superview and constrain it to it.
+    /// - Parameters:
+    ///   - subview: The Subview you want to add.
+    ///   - anchors: Array of ``LayoutAnchor`` describing the relation between the Superview and the Subview.
     func addSubview(_ subview: UIView, anchors: [LayoutAnchor]) {
         subview.translatesAutoresizingMaskIntoConstraints = false
         addSubview(subview)
         subview.activate(anchors: anchors, relativeTo: self)
     }
-
+    
+    /// Constraint a view to another item.
+    /// - Parameters:
+    ///   - anchors: Array of ``LayoutAnchor`` describing the relation between the view and the item.
+    ///   - item: The item based on which you want to constraint the view.
     func activate(anchors: [LayoutAnchor], relativeTo item: UIView? = nil) {
         let constraints = anchors.map { NSLayoutConstraint(from: self, to: item, anchor: $0) }
         NSLayoutConstraint.activate(constraints)
